a8a8d4f917117c080a9eaa60889c95b43dc2e554
luizpav <luizpavao35@gmail.com>
2025-12-12 13:29:43 -0300

    [CHORE] remove default header

--- app/(tabs)/Trophy/_layout.tsx
+++ app/(tabs)/Trophy/_layout.tsx
@@ -2,11 +2,10 @@ import { Stack } from "expo-router";
 
 export default function LayoutTrophy() {
   return (
-
-    <Stack screenOptions={{ }}>
-      <Stack.Screen name="index" options={{headerShown: false}}/>
-      <Stack.Screen name="Confronto" options={{ headerShown: false}}/>
-      <Stack.Screen name="Placar" />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="Confronto" />
+      <Stack.Screen name="HallOfFame" />
       <Stack.Screen name="Casas" />
       <Stack.Screen name="Modalidades" />
     </Stack>
