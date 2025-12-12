5bb8a8ca163f3094a4a9eca09ece207b9115c299
luizpav <luizpavao35@gmail.com>
2025-12-12 13:26:08 -0300

    [FEAT] Full connected to Firebase

--- app/(tabs)/Trophy/Confronto.tsx
+++ app/(tabs)/Trophy/Confronto.tsx
@@ -4,70 +4,25 @@ import {
   ImageBackground,
   ActivityIndicator,
   Modal,
-  TouchableOpacity,
   FlatList,
 } from "react-native";
-import { useState } from "react";
-import { AntDesign } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useRouter } from "expo-router";
+import { addDoc, collection, onSnapshot } from "firebase/firestore";
 
-import { useAuthUser } from "@/src/contexts/authUserContext";
-import { homes } from "@/src/types";
+import { db } from "@/src/firebaseConnection";
+import { useAuthUser } from "@/src/contexts/AuthUserContext";
+import { confronto } from "@/src/types";
 
 import ConfrontationField from "@/components/Confrontations/ConfrotationField";
 import NewConfrontation from "@/components/Confrontations/NewConfrontation";
+import Header from "@/components/Trophy/Header";
 
 export default function Confronto() {
-  const router = useRouter();
-
   const { userData, isLoading } = useAuthUser();
 
   const [editModalVisible, setEditModalVisible] = useState(false);
-  const [confrontos, setConfrontos] = useState([
-    {
-      id: (Date.now() + 3).toString(),
-      home1: homes.Arcania,
-      home2: homes.Spartta,
-      modalidade: "Basquete",
-      hour: "10:00",
-    },
-    {
-      id: (Date.now() + 2).toString(),
-      home1: homes.Alatares,
-      home2: homes.Atlantis,
-      modalidade: "Futsal",
-      hour: "11:00",
-    },
-    {
-      id: (Date.now() + 1).toString(),
-      home1: homes.Midgard,
-      home2: homes.Valhalla,
-      modalidade: "Volei",
-      hour: "12:00",
-    },
-    {
-      id: (Date.now() + 4).toString(),
-      home1: homes.Electra,
-      home2: homes.Extreme,
-      modalidade: "Queimado",
-      hour: "14:00",
-    },
-    {
-      id: (Date.now() + 5).toString(),
-      home1: homes.Ardharia,
-      home2: homes.Hunters,
-      modalidade: "Handebol",
-      hour: "16:00",
-    },
-    {
-      id: (Date.now() + 6).toString(),
-      home1: homes.Imperiais,
-      home2: homes.Monarcas,
-      modalidade: "Basquete",
-      hour: "17:00",
-    },
-  ]);
+  const [confrontos, setConfrontos] = useState<confronto[]>();
 
   if (isLoading) {
     return (
@@ -84,18 +39,58 @@ export default function Confronto() {
     setEditModalVisible(true);
   };
 
-  const addConfrontation = (
-    home1: homes,
-    home2: homes,
-    modalidade: string,
-    hour: string
-  ) => {
-    setConfrontos((prev) => [
-      ...prev,
-      { id: Date.now().toString(), home1, home2, modalidade, hour: hour },
-    ]);
-    console.log(confrontos);
-    setEditModalVisible(false);
+  useEffect(() => {
+    const docRef = collection(db, "Confrontos");
+    const getConfrontations = onSnapshot(docRef, (snapshot) => {
+      try {
+        if (!snapshot.empty) {
+          const confrontationsList = snapshot.docs.map((doc) => {
+            const data = doc.data() as confronto;
+            return {
+              ...data,
+            };
+          });
+
+          setConfrontos(confrontationsList);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+    return () => getConfrontations();
+  }, []);
+
+  const NewConfrontations = async ({
+    data,
+    local,
+    modalidade,
+    timeA,
+    timeB,
+    id,
+    status,
+  }: confronto) => {
+    try {
+      const dbRef = collection(db, "Confrontos");
+
+      const docRef = await addDoc(dbRef, {
+        data: data,
+        local: local,
+        modalidade: modalidade,
+        timeA: timeA,
+        timeB: timeB,
+        status: status,
+        id: id,
+      });
+      console.log(`confronto | ${docRef.id} | Criado!`);
+
+      setEditModalVisible(false);
+
+      return docRef.id;
+    } catch (error) {
+      console.error("Erro ao criar confronto: " + error);
+      throw error;
+    }
   };
 
   return (
@@ -104,36 +99,29 @@ export default function Confronto() {
         source={require("@/assets/images/Backgrounds/confrontosBackground.png")}
         className="flex-1"
       >
-        <View className="flex-row items-center pt-[20px] pl-[20px]">
-          <TouchableOpacity
-            onPress={() => {
-              router.back();
-            }}
-            className="mb-3"
-          >
-            <AntDesign name="arrowleft" size={24} color={"white"} />
-          </TouchableOpacity>
-          <Text className="w-[88%] text-3xl text-white text-center font-horizon">
-            Confrontos
-          </Text>
-
-          {userData?.admin && (
-            <TouchableOpacity onPress={openEditModal} className="mb-3">
-              <AntDesign name="edit" size={24} color={"white"} />
-            </TouchableOpacity>
-          )}
-        </View>
+        <Header
+          edit={true}
+          openEditModal={openEditModal}
+          isAdmin={userData?.admin}
+          title="Confrontos"
+        />
 
         <FlatList
           keyExtractor={(item) => item.id}
-          data={confrontos.sort((a, b) => {
-            return a.hour.localeCompare(b.hour);
+          data={confrontos?.sort((a, b) => {
+            return a.data
+              .toDate()
+              .toLocaleString()
+              .localeCompare(b.data.toDate().toLocaleString());
           })}
           renderItem={({ item }) => (
             <ConfrontationField
-              home1={item.home1}
-              home2={item.home2}
-              hour={item.hour}
+              home1={item.timeA}
+              home2={item.timeB}
+              hour={item.data
+                .toDate()
+                .toLocaleTimeString()
+                .replace(/:\d{2}$/, "")}
               modalidade={item.modalidade}
             />
           )}
@@ -147,7 +135,7 @@ export default function Confronto() {
         >
           <NewConfrontation
             closeModal={() => setEditModalVisible(false)}
-            addConfrontation={addConfrontation}
+            addConfrontation={NewConfrontations}
           />
         </Modal>
       </ImageBackground>
