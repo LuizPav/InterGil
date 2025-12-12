6839560051b8608e750511b51c248178e5124aaf
luizpav <luizpavao35@gmail.com>
2025-12-12 14:52:26 -0300

    [STYLES] Added elevation for shadow aspect

--- components/TabBar/TabBar.tsx
+++ components/TabBar/TabBar.tsx
@@ -22,7 +22,7 @@ export default function TabBar(props: BottomTabBarProps) {
   });
 
   return (
-    <View className="flex-row rounded-full absolute bottom-10 inset-x-10 bg-white elevation-10 px-4 py-2">
+    <View className="flex-row rounded-full absolute bottom-10 inset-x-10 bg-white elevation-xl px-4 py-2">
       {sortedRoutes.map((route, index) => {
         const { options } = descriptors[route.key];
         const label =
